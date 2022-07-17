814b23b4f254fa7148d2b504c76640799ddf568d
AlexandruPodosu <Podosu_alexandru@yahoo.com>
2022-07-17 19:10:22 +0300

    add background, slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatInputModule} from '@angular/material/input'; //input
-import {MatToolbarModule} from '@angular/material/toolbar'; //toolbar
-import {MatButtonModule} from '@angular/material/button'; //button
+import {MatInputModule} from '@angular/material/input'; 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'; // pt verificare email
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'; //
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { routingcComponents } from './app-routing.module';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import {MatDividerModule} from '@angular/material/divider';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 
 @NgModule({
@@ -42,7 +43,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatCardModule,
     MatProgressBarModule,
     MatDividerModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgImageSliderModule
   ],
   //servicii
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,20 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title:string = "Home title";
-  buttonDisabled:boolean = false;
-  color:string ="green";
-  newTitle: String = "test";
+  imageObject = [{
+    image: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    thumbImage: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+   
+}, {
+    image: 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    thumbImage: 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+}, {
+    image: 'https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+    thumbImage: 'https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+},
+{
+  image: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  thumbImage: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+},
+{
+  image: 'https://images.unsplash.com/photo-1544716278-e513176f20b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  thumbImage: 'https://images.unsplash.com/photo-1544716278-e513176f20b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+},
+{
+  image: 'https://images.unsplash.com/photo-1588666309990-d68f08e3d4a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80',
+  thumbImage: 'https://images.unsplash.com/photo-1588666309990-d68f08e3d4a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80',
+}
+
+];
   constructor() { }
 
   ngOnInit(): void {
   }
-  changeButton() : void{
-    
-    this.color= "red";
-  }
-  changeTitle():void 
-  {
-    this.title ="New title";
-  }
+
+ 
 }
